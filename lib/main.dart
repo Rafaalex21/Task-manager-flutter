bee313d8e2b29b68d9318832470a0531353bfd7e
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-11-28 19:06:17 -0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,46 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Row(
+      home: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Container(
-          color: Colors.redAccent,
-          width: 100,
-          height: 100,
-        ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
-          ),],
-
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [Container(
+              color: Colors.redAccent,
+              width: 100,
+              height: 100,
+            ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              ),],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+            ),
+              Container(
+                color: Colors.redAccent,
+                width: 50,
+                height: 50,
+              ),],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyanAccent,height: 50,width: 50,),
+              Container(color: Colors.pink,height: 50,width: 50,),
+              Container(color: Colors.deepPurpleAccent,height: 50,width: 50,),
+            ],
+          )
+        ],
       ),
     );
   }
