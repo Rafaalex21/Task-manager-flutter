7c349e820941c957778c39fc125fcb350c3533a3
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-11-29 20:26:59 -0300

    eleventh commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +26,49 @@ class MyApp extends StatelessWidget {
           leading: Container(),
           title: Text('Tarefas'),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter','https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
-            Task('Aprender Italiano','https://thumbs.dreamstime.com/b/vetor-de-bandeira-italiano-nacional-do-pa%C3%ADs-it%C3%A1lia-wallpaper-italiana-pano-fundo-da-196750105.jpg'),
-            Task('Adquirir o shape inexplicavel','https://media.gettyimages.com/id/3238896/pt/foto/austrian-born-bodybuilder-arnold-schwarzenegger-points-one-hand-out-as-he-flexes-his-torso-on.jpg?s=612x612&w=gi&k=20&c=cJtR1oJnFXI7czR2hdd-wE9-T7WFeMflJn8AFQAkDLw='),
-            Task('Reconquistar Jerusalem','https://blogdoenem.com.br/wp-content/uploads/2016/07/1-1.jpg'),
-            Task('Lamentar pela Queda de Constantinopla','https://s1.static.brasilescola.uol.com.br/be/conteudo/images/tomada-constantinopla-por-mehmed-ii-56bcbc2c8d88b.jpg'),
-            Task('Reconstruir Constantinopla','https://s2.glbimg.com/ARg-ud6GCQlFLcGx1JxTBHRlCok=/e.glbimg.com/og/ed/f/original/2019/11/01/constantinopla.jpg'),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 900),
+          child: ListView(
+            children: [
+              Task(
+                  'Aprender Flutter',
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  2),
+              Task(
+                  'Aprender Italiano',
+                  'https://thumbs.dreamstime.com/b/vetor-de-bandeira-italiano-nacional-do-pa%C3%ADs-it%C3%A1lia-wallpaper-italiana-pano-fundo-da-196750105.jpg',
+                  3),
+              Task(
+                  'Adquirir o shape inexplicavel',
+                  'https://media.gettyimages.com/id/3238896/pt/foto/austrian-born-bodybuilder-arnold-schwarzenegger-points-one-hand-out-as-he-flexes-his-torso-on.jpg?s=612x612&w=gi&k=20&c=cJtR1oJnFXI7czR2hdd-wE9-T7WFeMflJn8AFQAkDLw=',
+                  4),
+              Task(
+                  'Reconquistar Jerusalem',
+                  'https://blogdoenem.com.br/wp-content/uploads/2016/07/1-1.jpg',
+                  5),
+              Task(
+                  'Lamentar pela Queda de Constantinopla',
+                  'https://s1.static.brasilescola.uol.com.br/be/conteudo/images/tomada-constantinopla-por-mehmed-ii-56bcbc2c8d88b.jpg',
+                  1),
+              Task(
+                  'Reconstruir Constantinopla',
+                  'https://s2.glbimg.com/ARg-ud6GCQlFLcGx1JxTBHRlCok=/e.glbimg.com/og/ed/f/original/2019/11/01/constantinopla.jpg',
+                  5),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+
+            });
+
+
+          },
+          child: Icon(Icons.remove_red_eye),
+        ),
       ),
     );
   }
@@ -38,8 +77,10 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String nome;
   final String foto;
+  final int dificuldade;
 
-  const Task(this.nome,this.foto, {Key? key}) : super(key: key);
+  const Task(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -56,24 +97,36 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black26,
+                        ),
                         height: 100,
                         width: 110,
-                        child: Image.network(
-                          widget.foto,
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       Column(
@@ -91,15 +144,43 @@ class _TaskState extends State<Task> {
                               )),
                           Row(
                             children: [
-                              Icon(Icons.star,size: 15,color: Colors.blue,),
-                              Icon(Icons.star,size: 15,color: Colors.blue,),
-                              Icon(Icons.star,size: 15,color: Colors.blue,),
-                              Icon(Icons.star,size: 15,color: Colors.blue[100],),
-                              Icon(Icons.star,size: 15,color: Colors.blue[100],),
-
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
                             ],
                           )
-
                         ],
                       ),
                       Container(
@@ -136,7 +217,9 @@ class _TaskState extends State<Task> {
                       child: Container(
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel / 10,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
                         ),
                         width: 200,
                       ),
