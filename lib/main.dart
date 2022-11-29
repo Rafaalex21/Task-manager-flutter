b81aeba16bd639f2a843ee85fd1bf1a47cf22812
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-11-29 16:32:55 -0300

    sixth commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,50 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(color: Colors.blue,height: 140,),
-              Container(color: Colors.white,height: 100,child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(color: Colors.black,height: 100,width: 72,),Text('Aprender Flutter'),
-                  ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-                ],
-              ),),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task('Aprender Flutter'),
+            Task('Aprender Italiano'),
+            Task('Adquirir o shape inexplicavel'),
+          ],
         ),
-        floatingActionButton: FloatingActionButton( onPressed:(){}),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome,{Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  width: 72,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
