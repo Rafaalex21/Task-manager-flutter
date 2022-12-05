df2ded58f9c32d1d7b5ae846495882cb1dfe909d
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-12-05 19:59:12 -0300

    new commit

--- lib/screens/Initial_Screen.dart
+++ lib/screens/Initial_Screen.dart
@@ -1,8 +1,8 @@
+import 'package:first_flutter_project/components/Task.dart';
+import 'package:first_flutter_project/data/task_dao.dart';
 import 'package:first_flutter_project/data/task_inherited.dart';
 import 'package:first_flutter_project/screens/form_screen.dart';
 import 'package:flutter/material.dart';
-
-
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
@@ -19,16 +19,82 @@ class _InitialScreenState extends State<InitialScreen> {
         leading: Container(),
         title: const Text('Tarefas'),
       ),
-      body: ListView(
-        children: TaskInherited.of(context).taskList,
-        padding: EdgeInsets.only(top: 8,bottom: 70),
+      body: Padding(
+        padding: EdgeInsets.only(top: 8, bottom: 70),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.active:
+                  return Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task tarefa = items[index];
+                            return tarefa;
+                          });
+                    }
+                    return Center(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            size: 128,
+                          ),
+                          Text(
+                            'Não há nenhuma tarefa',
+                            style: TextStyle(fontSize: 32),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                  return Text('Erro ao carregar Tarefas');
+                  break;
+              }
+              return Text('Unknow error');
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (contextNew) => FormScreen(taskContext: context,),
+              builder: (contextNew) => FormScreen(
+                taskContext: context,
+              ),
             ),
           );
         },
